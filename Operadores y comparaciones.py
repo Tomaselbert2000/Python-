792474f8a622bd44575ccc8b1fbe6d9960a2fc7e
Tomaselbert2000 <59849950+Tomaselbert2000@users.noreply.github.com>
2020-01-22 17:42:06 -0300

    Add files via upload

---
+++ Operadores y comparaciones.py
@@ -0,0 +1,40 @@
+"""
+Fecha de inicio: 21 de enero de 2020.
+Autor: Tomas Elbert.
+Finalidad: Ejemplos sencillos sobre el funcionamiento de los operadores y las comparaciones.
+"""
+### A continuacion se muestran ejemplos sencillos sobre los operadores y como se escriben cada uno ###
+### Operador de suma + ###
+a=12+76
+print(a)
+### Operador de resta - ###
+b=55-41
+print(b)
+### Operador de multiplicaion * ###
+c=16*5
+print(c)
+### Operador de division / ###
+d= 80/4
+print(d)
+### Modulo, obtiene el resto de la division % ###
+e=76%6
+print(e)
+### Exponencial, obtiene potencia de un numero ** ###
+f=4**5
+print(f)
+### Division entera entre valores // ###
+g=100//25
+print(g)
+### A continuacion se muestran ejemplos basicos acerca de las comparaciones ###
+### Ejemplo de como comprobar una igualdad ###
+A=(10==10)
+print(A)
+B=(10==20)
+print(B)
+### Ejemplo aplicando el operador != para valores que son diferentes ###
+C=(20!=90)
+print(C)
+### Signos de mayor y menor ###
+D=10<20
+print(D)
+### Los signos de >=< funcionan de la misma manera
